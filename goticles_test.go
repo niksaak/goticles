430db04b7718ba7a117a4cfbddc5b033c1d75eb7
Mikola Samardak <niksaak@gmail.com>
2014-05-25 03:59:57 +0300

    update and fix tests, gofmt

--- goticles_test.go
+++ goticles_test.go
@@ -12,36 +12,23 @@ func rf64() float64 {
 }
 
 func TestOneBodyPositionChange(t *testing.T) {
-	const STEP = 1.0/1
-	SuperVerbose = true
+	const STEP = 1.0 / 1
 	space := MkSpace()
+	p := space.MkParticle(1)
 	if count := len(space.Particles); count != 1 {
 		t.Errorf("particle count is not 1 but %d", count)
 	}
-	p := space.MkParticle(1)
 	p.SetPosition(vect.V{0, 0})
 	p.SetVelocity(vect.V{1, 0})
 	t.Logf("%.4f: %v", 0.0, p)
-	/*
-	for c := 0.0; c < 1; c += STEP {
+	for c := 0.0; c < 1+G; c += STEP {
 		space.Step(STEP)
 		t.Logf("%.4f: %v", c, p)
 	}
-	*/
-	c := 0.0
-	for {
-		space.Step(STEP)
-		t.Logf("%.4f: %v", c, p)
-		if c > 1 {
-			break
-		}
-		c += STEP
-	}
-	SuperVerbose = false
 }
 
 func TestTwoBodyPositionChanges(t *testing.T) {
-	const STEP = 1.0/60
+	const STEP = 1.0 / 60
 	space := MkSpace()
 	p1 := space.MkParticle(1)
 	p1.SetPosition(vect.V{0, 0})
@@ -56,8 +43,8 @@ func TestTwoBodyPositionChanges(t *testing.T) {
 	t.Log(p2)
 }
 
-func BenchmarkSpace(b *testing.B) {
-	const STEP = 1.0/60
+func BenchmarkSpace1(b *testing.B) {
+	const STEP = 1.0 / 60
 	space := MkSpace()
 	particle := space.MkParticle(1)
 	particle.SetVelocity(vect.V{1, 0})
@@ -65,12 +52,12 @@ func BenchmarkSpace(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		space.Step(STEP)
 	}
-	b.Logf("t: %.4f; p: %v", space.time, particle)
+	b.Logf("t: %.4f; p: %v", space.Time, particle)
 }
 
 func BenchmarkSpace512(b *testing.B) {
 	const PARTICLE_COUNT = 512
-	const STEP = 1.0/60
+	const STEP = 1.0 / 60
 	space := MkSpace()
 	for i := 0; i < PARTICLE_COUNT; i++ {
 		particle := space.MkParticle(1)
@@ -81,21 +68,21 @@ func BenchmarkSpace512(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		space.Step(STEP)
 	}
-	b.Logf("t: %.4f; p: %v", space.time, particle)
-}
-func BenchmarkSpace1024(b *testing.B) {
-	const PARTICLE_COUNT = 1024
-	const STEP = 1.0/60
-	space := MkSpace()
-	for i := 0; i < PARTICLE_COUNT; i++ {
-		particle := space.MkParticle(1)
-		particle.SetVelocity(vect.V{rf64(), rf64()})
-	}
-	particle := &space.Particles[0]
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		space.Step(STEP)
-	}
-	b.Logf("t: %.4f; p: %v", space.time, particle)
+	b.Logf("t: %.4f; p: %v", space.Time, particle)
 }
 
+func BenchmarkSpace1024(b *testing.B) {
+	const PARTICLE_COUNT = 1024
+	const STEP = 1.0 / 60
+	space := MkSpace()
+	for i := 0; i < PARTICLE_COUNT; i++ {
+		particle := space.MkParticle(1)
+		particle.SetVelocity(vect.V{rf64(), rf64()})
+	}
+	particle := &space.Particles[0]
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		space.Step(STEP)
+	}
+	b.Logf("t: %.4f; p: %v", space.Time, particle)
+}
