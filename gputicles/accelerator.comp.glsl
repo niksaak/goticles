53c2e68f3346031c00770f6f2cb90a87be3b0943
Mikola Samardak <niksaak@gmail.com>
2014-09-07 22:38:16 +0300

    gputicles: finish implementing, switch to leapfrog integrator.

--- gputicles/accelerator.comp.glsl
+++ gputicles/accelerator.comp.glsl
@@ -1,41 +1,42 @@
 #version 440
 
 struct Particle {
-	double vec2 position;
-	double vec2 velocity;
-	double vec2 acceleration;
-	double mass;
+	vec2 position;
+	vec2 acceleration;
+	float mass;
 };
 
 layout(local_size_x=256) in;
 
-layout(std430) buffer particles {
+layout(std430, binding=0) buffer particles {
 	Particle p[];
 };
 
-const G        = 6.67384e-11
-const DBL_MIN  = 2.2250738585072014e-308;
-const TRESHOLD = 2e-3;
+const float G        = 6.67384e-11;
+const float DBL_MIN  = 2.2250738585072014e-308;
+const float TRESHOLD = 2e-3;
 
 void main() {
 	int N = int(gl_NumWorkGroups.x*gl_WorkGroupSize.x);
 	int id = int(gl_GlobalInvocationID);
 
-	double vec2 position = p[id].position;
-	double vec2 acceleration = vec2(0, 0);
-	double mass = p[id].mass;
+	vec2 position = p[id].position;
+	vec2 acceleration = vec2(0, 0);
+	float mass = p[id].mass;
 
 	// accelerate
 	for (int j = 0; j < N; ++j) {
-		double vec2 other = p[j].position;
-		double vec2 vDist = position - other;
-		double vec2 dist = length(dist);
-		dist *= dist;
-		if dist < TRESHOLD {
+		if (j == id) {
 			continue;
 		}
-		double vec2 uDist = normalize(vDist)
-		acceleration += -(G * mass * p[j].mass * pow(dist, 2) * uDist);
+		vec2 other = p[j].position;
+		vec2 vDist = position - other;
+		float dist = length(vDist);
+		if (dist < TRESHOLD) {
+			continue;
+		}
+		vec2 uDist = normalize(vDist);
+		acceleration -= G * mass * p[j].mass / (dist * dist) * uDist;
 	}
 	p[id].acceleration = acceleration;
 }
--- gputicles/gputicles.go
+++ gputicles/gputicles.go
@@ -1,46 +1,47 @@
 package gputicles
 
 import (
+	"fmt"
 	"github.com/go-gl/glow/gl-core/4.4/gl"
+	"github.com/niksaak/goticles"
 	"github.com/niksaak/goticles/demo/engine"
 	"github.com/niksaak/goticles/vect"
-	"github.com/niksaak/goticles"
 	"reflect"
 	"unsafe"
-	"fmt"
 )
 
 const (
 	WORK_GROUP_LOCAL_SIZE = 256
-	G                     = 6.67384e-11
 )
 
 type Space struct {
 	Particles []goticles.P
-	particles [4][]particle
 
 	accelProgram uint32
 	vao          uint32
 	vbo          uint32
 }
 
-type particle struct {
-	position     vect.V
-	velocity     vect.V
-	acceleration vect.V
-	mass         float64
+type gpuParticle struct {
+	position     [2]float32
+	acceleration [2]float32
+	mass         float32
+	_            [3]float32
 }
 
 func New() (s *Space, err error) {
+	s = new(Space)
 	s.accelProgram, err = loadShader()
 	if err != nil {
 		return nil, err
 	}
-
 	gl.GenVertexArrays(1, &s.vao)
 	gl.GenBuffers(1, &s.vbo)
 
-	s = new(Space)
+	gl.UseProgram(s.accelProgram)
+	gl.BindVertexArray(s.vao)
+	gl.BindBufferBase(gl.SHADER_STORAGE_BUFFER, 0, s.vbo)
+
 	return s, nil
 }
 
@@ -50,9 +51,6 @@ func (s *Space) MkParticle(mass float64) *goticles.P {
 		Id:   id,
 		Mass: mass,
 	})
-	for i := range s.particles {
-		s.particles[i] = append(s.particles[i], particle{})
-	}
 	return &s.Particles[id]
 }
 
@@ -65,64 +63,52 @@ func (s *Space) Step(dt float64) {
 	gl.GetIntegerv(gl.CURRENT_PROGRAM, &prevProgram)
 	gl.UseProgram(s.accelProgram)
 	gl.BindVertexArray(s.vao)
-	gl.EnableVertexAttribArray(0)
-	gl.BindBuffer(gl.ARRAY_BUFFER, s.vbo)
+	gl.BindBufferBase(gl.SHADER_STORAGE_BUFFER, 0, s.vbo)
 
-	s.evaluate1()
-	s.evaluateK(dt/2, 1)
-	s.evaluateK(dt/2, 2)
-	s.evaluateK(dt, 3)
-	s.applyState()
+	gl.BufferData(
+		gl.SHADER_STORAGE_BUFFER,
+		len(s.Particles)*int(unsafe.Sizeof(gpuParticle{})),
+		nil,
+		gl.DYNAMIC_COPY)
+	{ // send
+		mappedParticles, err := sliceMapParticlesBuffer(gl.WRITE_ONLY, len(s.Particles))
+		if err != nil {
+			panic(err)
+		}
+		for i, p := range s.Particles {
+			mappedParticles[i] = gpuParticle{
+				position: [2]float32{float32(p.Position.X), float32(p.Position.Y)},
+				mass:     float32(p.Mass),
+			}
+		}
+		gl.UnmapBuffer(gl.SHADER_STORAGE_BUFFER)
+	}
+	gl.DispatchCompute(uint32(len(s.Particles))/WORK_GROUP_LOCAL_SIZE, 1, 1)
+	gl.MemoryBarrier(gl.SHADER_STORAGE_BARRIER_BIT)
+	{ // receive
+		mappedParticles, err := sliceMapParticlesBuffer(gl.READ_ONLY, len(s.Particles))
+		if err != nil {
+			panic(err)
+		}
+		for i, q := range mappedParticles {
+			p := &s.Particles[i]
+			p.Position = p.Position.Add(p.Velocity.Mul(dt)).Add(
+				p.Acceleration.Mul(dt * dt).Mul(0.5))
+			oldAcceleration := p.Acceleration
+			p.Acceleration = vect.V{
+				float64(q.acceleration[0]), float64(q.acceleration[1]),
+			}
+			p.Velocity = p.Velocity.Add(
+				p.Acceleration.Add(oldAcceleration).Mul(0.5).Mul(dt))
+		}
+		gl.UnmapBuffer(gl.SHADER_STORAGE_BUFFER)
+	}
 
 	gl.UseProgram(uint32(prevProgram))
 }
 
-func (s *Space) evaluate1() {
-	for i, p := range s.Particles {
-		s.particles[0][i] = particle{
-			position: p.Position,
-			velocity: p.Velocity,
-			mass:     p.Mass,
-		}
-	}
-}
-
-func (s *Space) evaluateK(dt float64, k int) {
-	count := int(uintptr(len(s.particles[0]))*unsafe.Sizeof(particle{}))
-	gl.BufferData(
-		gl.ARRAY_BUFFER,
-		count,
-		nil,
-		gl.DYNAMIC_COPY)
-	{
-		mappedParticles, err := sliceMapParticlesBuffer(gl.WRITE_ONLY, count)
-		if err != nil {
-			panic(err)
-		}
-		copy(mappedParticles, s.particles[k-1])
-		gl.UnmapBuffer(gl.ARRAY_BUFFER)
-	}
-	gl.DispatchCompute(uint32(count / WORK_GROUP_LOCAL_SIZE), 1, 1) // accelerations
-	gl.Finish()
-	{
-		mappedParticles, err := sliceMapParticlesBuffer(gl.READ_ONLY, count)
-		if err != nil {
-			panic(err)
-		}
-		copy(s.particles[k], mappedParticles)
-		gl.UnmapBuffer(gl.ARRAY_BUFFER)
-	}
-	for i := range s.particles[k] {
-		p := &s.particles[k][i]
-		prev := &s.particles[k-1][i]
-		start := &s.particles[0][i]
-		p.position = start.position.Add(prev.velocity.Mul(dt))
-		p.velocity = start.velocity.Add(prev.acceleration.Mul(dt))
-	}
-}
-
-func sliceMapParticlesBuffer(mode uint32, length int) ([]particle, error) {
-	data := gl.MapBuffer(gl.ARRAY_BUFFER, gl.WRITE_ONLY)
+func sliceMapParticlesBuffer(mode uint32, length int) ([]gpuParticle, error) {
+	data := gl.MapBuffer(gl.SHADER_STORAGE_BUFFER, mode)
 	if data == nil {
 		return nil, fmt.Errorf("buffer data pointer is nil")
 	}
@@ -131,36 +117,8 @@ func sliceMapParticlesBuffer(mode uint32, length int) ([]particle, error) {
 		Len:  length,
 		Cap:  length,
 	}
-	slice := (*[]particle)(unsafe.Pointer(&header))
+	slice := (*[]gpuParticle)(unsafe.Pointer(&header))
 	return *slice, nil
-}
-
-func (s *Space) applyState() {
-	for i := range s.Particles {
-		p := &s.Particles[i]
-		p.Position = rk4mean(
-			s.particles[0][i].position,
-			s.particles[1][i].position,
-			s.particles[2][i].position,
-			s.particles[3][i].position)
-		p.Velocity = rk4mean(
-			s.particles[0][i].velocity,
-			s.particles[1][i].velocity,
-			s.particles[2][i].velocity,
-			s.particles[3][i].velocity)
-		p.Acceleration = rk4mean(
-			s.particles[0][i].acceleration,
-			s.particles[1][i].acceleration,
-			s.particles[2][i].acceleration,
-			s.particles[3][i].acceleration)
-	}
-}
-
-func rk4mean(k1, k2, k3, k4 vect.V) vect.V {
-	return vect.V{
-		X: (1.0 / 6.0) * (k1.X + 2*(k2.X+k3.X) + k4.X),
-		Y: (1.0 / 6.0) * (k1.Y + 2*(k2.Y+k3.Y) + k4.Y),
-	}
 }
 
 func loadShader() (shader uint32, err error) {
